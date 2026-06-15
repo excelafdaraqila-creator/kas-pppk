0362138e5517b07bf17a0f486ade50d3ac58bb9f
vd <excelafdaraqila@gmail.com>
2026-06-16 01:25:28 +0700

    Update data_kas_pppk.js

--- data_kas_pppk.js
+++ data_kas_pppk.js
@@ -102,7 +102,7 @@ const kasData = [
   {tanggal:"03-04-2026",nama:"Liska",uraian:"Iuran PPPK",masuk:25000,keluar:0},
 
   // ===== PENGELUARAN 01 JUNI 2026 =====
-  {tanggal:"01-06-2026",nama:"Kas PPPK",uraian:"Santunan meninggal orang tua Ibu Anis SDN Neglasari",masuk:0,keluar:400000}
+  {tanggal:"01-06-2026",nama:"Kas PPPK",uraian:"Santunan meninggal orang tua Ibu Anis SDN Neglasari",masuk:0,keluar:400000},
 
   // ===== PEMASUKAN 15 JUNI 2026 =====
   {tanggal:"15-06-2026",nama:"Nina Napisah",uraian:"Iuran PPPK",masuk:25000,keluar:0},
