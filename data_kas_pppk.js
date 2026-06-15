63995929d0771083b3d164a0064eb00e19b9edda
vd <excelafdaraqila@gmail.com>
2026-06-16 01:29:26 +0700

    Update data_kas_pppk.js

--- data_kas_pppk.js
+++ data_kas_pppk.js
@@ -95,10 +95,10 @@ const kasData = [
   {tanggal:"15-02-2026",nama:"Wildan Ramdani",uraian:"Iuran PPPK",masuk:25000,keluar:0},
   {tanggal:"15-02-2026",nama:"Erlan Suherlan",uraian:"Iuran PPPK",masuk:25000,keluar:0},
 
-  // ===== PENGELUARAN 08 MARET 2026 =====
+    // ===== PENGELUARAN 08 MARET 2026 =====
   {tanggal:"08-03-2026",nama:"Kas PPPK",uraian:"Santunan meninggal Ibu dari Reni Susana",masuk:0,keluar:400000},
 
-  // ===== TAMBAHAN BARU =====
+  // ===== PEMASUKAN 03 APRIL 2026 =====
   {tanggal:"03-04-2026",nama:"Liska",uraian:"Iuran PPPK",masuk:25000,keluar:0},
 
   // ===== PENGELUARAN 01 JUNI 2026 =====
